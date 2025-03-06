4edf5845cc4c6d04b6d0e48e0df0671ad13bed3e
Sneha_Dey <sneha_dey@epam.com>
2025-03-06 12:14:03 +0530

    Updated

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -1,8 +1,1 @@
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
-};
+exports.handler = async (event) => {        return {            statusCode: 200,            body: JSON.stringify({statusCode:200, message: "Hello from Lambda" }), // Removed duplicate statusCode        };};
